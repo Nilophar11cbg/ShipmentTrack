b4e486f744f83c386e4d13816f9a015ccea2a265
Nilophar <nilophars@cybage.com>
2024-11-19 17:49:17 +0530

    Add files via upload

--- src/main/java/com/shipment/track/repository/ShipmentDetailsRepository.java
+++ src/main/java/com/shipment/track/repository/ShipmentDetailsRepository.java
@@ -1,14 +1,15 @@
 package com.shipment.track.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shipment.track.model.ShipmentDetails;
 
 public interface ShipmentDetailsRepository extends JpaRepository<ShipmentDetails, Long> {
 	 
-	// Long getShipmentByStatus(String status);
+	 List<ShipmentDetails> getShipmentByStatus(String status);
 	 
-	// ShipmentDetails getShipmentDetails(Long shipmentId);
 	 
 }
