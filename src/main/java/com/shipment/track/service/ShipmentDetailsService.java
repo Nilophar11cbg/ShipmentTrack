b0f6fae5519ddd5c7329012951e4b536d8743cf5
Nilophar <nilophars@cybage.com>
2024-11-19 17:49:57 +0530

    Add files via upload

--- src/main/java/com/shipment/track/service/ShipmentDetailsService.java
+++ src/main/java/com/shipment/track/service/ShipmentDetailsService.java
@@ -1,6 +1,7 @@
 package com.shipment.track.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,18 @@ public class ShipmentDetailsService {
 
 	@Autowired
 	ShipmentDetailsRepository repository;
-	
-	 public List<ShipmentDetails> getAllShipmentDetails() {
-		return repository.findAll();
-	 }
-	 
-		/*
-		 * public Long getShipmentByStatus(String status) { return
-		 * repository.getShipmentByStatus(status); }
-		 * 
-		 * public ShipmentDetails getShipmentDetailsById(Long shipmentId) { return
-		 * repository.getShipmentDetails(shipmentId); }
-		 */
 
+	public List<ShipmentDetails> getAllShipmentDetails() {
+		return repository.findAll();
+	}
+
+	public List<ShipmentDetails> getShipmentByStatus(String status) {
+		return repository.getShipmentByStatus(status);
+	}
+
+	public Optional<ShipmentDetails> getShipmentDetailsById(long shipmentId) {
+		return repository.findById(shipmentId);
+	}
+
+	
 }
