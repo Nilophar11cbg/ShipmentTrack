576f77a326a3728158bba5943f14e8afba9a4602
Nilophar <nilophars@cybage.com>
2024-11-19 17:48:07 +0530

    Add files via upload

--- src/main/java/com/shipment/track/controller/ShipmentDetailsController.java
+++ src/main/java/com/shipment/track/controller/ShipmentDetailsController.java
@@ -2,14 +2,17 @@ package com.shipment.track.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shipment.track.model.ShipmentCountResponse;
 import com.shipment.track.model.ShipmentDetails;
 import com.shipment.track.service.ShipmentDetailsService;
 
@@ -18,21 +21,41 @@ import com.shipment.track.service.ShipmentDetailsService;
 public class ShipmentDetailsController {
 
 	@Autowired
-	ShipmentDetailsService shipmentDetailsService; 
-	
-	
+	ShipmentDetailsService shipmentDetailsService;
+
 	@GetMapping("/shipments")
 	public ResponseEntity<List<ShipmentDetails>> getAllShipmentDetails() {
 		try {
 			List<ShipmentDetails> shipments = new ArrayList<>();
 
-			shipmentDetailsService.getAllShipmentDetails().forEach(shipments::add);
+			if (shipments.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
 
+			shipmentDetailsService.getAllShipmentDetails().forEach(shipments::add);
 			return new ResponseEntity<>(shipments, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
 
+	@GetMapping("/shipments/{id}")
+	public ResponseEntity<ShipmentDetails> getShipmentById(@PathVariable("id") long id) {
+		Optional<ShipmentDetails> tutorialData = shipmentDetailsService.getShipmentDetailsById(id);
+
+		if (tutorialData.isPresent()) {
+			return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	
+	  @GetMapping("/shipments/status/{status}") public
+	  ResponseEntity<ShipmentCountResponse> getShipmentByStatus(@PathVariable("status") String status) {
+			List<ShipmentDetails> lst = shipmentDetailsService.getShipmentByStatus(status);
+			ShipmentCountResponse response = new ShipmentCountResponse(lst.size());
+			return ResponseEntity.ok(response);
+		}
+	 
 }
