d81a2e5b0ec9a0577e6e8c0dfe621521e1801a84
Nilophar <nilophars@cybage.com>
2024-11-27 11:58:23 +0530

    Add files via upload

--- src/main/java/com/shipment/track/controller/ShipmentDetailsController.java
+++ src/main/java/com/shipment/track/controller/ShipmentDetailsController.java
@@ -9,11 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shipment.track.model.ShipmentCountResponse;
 import com.shipment.track.model.ShipmentDetails;
+import com.shipment.track.service.EmailService;
 import com.shipment.track.service.ShipmentDetailsService;
 
 @RestController
@@ -22,6 +24,9 @@ public class ShipmentDetailsController {
 
 	@Autowired
 	ShipmentDetailsService shipmentDetailsService;
+
+	@Autowired
+	private EmailService emailService;
 
 	@GetMapping("/shipments")
 	public ResponseEntity<List<ShipmentDetails>> getAllShipmentDetails() {
@@ -51,11 +56,25 @@ public class ShipmentDetailsController {
 	}
 
 	
-	  @GetMapping("/shipments/status/{status}") public
-	  ResponseEntity<ShipmentCountResponse> getShipmentByStatus(@PathVariable("status") String status) {
-			List<ShipmentDetails> lst = shipmentDetailsService.getShipmentByStatus(status);
-			ShipmentCountResponse response = new ShipmentCountResponse(lst.size());
-			return ResponseEntity.ok(response);
+	@GetMapping("/shipments/status/{status}")
+	public ResponseEntity<ShipmentCountResponse> getShipmentByStatus(@PathVariable("status") String status) {
+		List<ShipmentDetails> lst = shipmentDetailsService.getShipmentByStatus(status);
+		ShipmentCountResponse response = new ShipmentCountResponse(lst.size());
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/shipment")
+	public ResponseEntity<String> createShipmentDetail() {
+		try {
+			
+			//read email contents and insert data 
+			emailService.fetchEmails();
+			
+			return new ResponseEntity<>("Shipment created", HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+		
+	}
 	 
-}
