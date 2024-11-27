c167e4bc8cd97da47e2f9e2fe709e0624202d27d
Nilophar Mubin Shaikh <nilophars@cybage.com>
2024-11-27 11:53:14 +0530

    model added

---
+++ src/main/java/com/shipment/track/model/ShipmentCountResponse.java
@@ -0,0 +1,18 @@
+package com.shipment.track.model;
+
+public class ShipmentCountResponse {
+	 private long count;
+
+	    public ShipmentCountResponse(long count) {
+	        this.count = count;
+	    }
+
+	    // Getter and Setter
+	    public long getCount() {
+	        return count;
+	    }
+
+	    public void setCount(long count) {
+	        this.count = count;
+	    }
+}
