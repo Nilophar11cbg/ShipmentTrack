b119d7ff5bc2912cfe141bdfdb8e494bc9284e8e
Nilophar <nilophars@cybage.com>
2024-11-28 10:58:19 +0530

    Javadoc added

--- src/main/java/com/shipment/track/service/ShipmentDetailsService.java
+++ src/main/java/com/shipment/track/service/ShipmentDetailsService.java
@@ -9,23 +9,38 @@ import org.springframework.stereotype.Service;
 import com.shipment.track.model.ShipmentDetails;
 import com.shipment.track.repository.ShipmentDetailsRepository;
 
+/**
+ * 
+ */
 @Service
 public class ShipmentDetailsService {
 
 	@Autowired
 	ShipmentDetailsRepository repository;
 
+	/**
+	 * This method gets all shipment details
+	 * @return
+	 */
 	public List<ShipmentDetails> getAllShipmentDetails() {
 		return repository.findAll();
 	}
 
+	/**
+	 * This method gets shipment details as per status provided
+	 * @param status
+	 * @return
+	 */
 	public List<ShipmentDetails> getShipmentByStatus(String status) {
 		return repository.getShipmentByStatus(status);
 	}
 
+	/**
+	 * This method gets shipment details as per status shipment id
+	 * @param shipmentId
+	 * @return
+	 */
 	public Optional<ShipmentDetails> getShipmentDetailsById(long shipmentId) {
 		return repository.findById(shipmentId);
 	}
-
-	
 }
