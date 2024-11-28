6b5c73b76839ec6ad25b4f481da0d92fda3f874e
Nilophar <nilophars@cybage.com>
2024-11-28 10:57:26 +0530

    Javadoc added

--- src/main/java/com/shipment/track/controller/ShipmentDetailsController.java
+++ src/main/java/com/shipment/track/controller/ShipmentDetailsController.java
@@ -7,9 +7,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +18,12 @@ import com.shipment.track.model.ShipmentDetails;
 import com.shipment.track.service.EmailService;
 import com.shipment.track.service.ShipmentDetailsService;
 
+/**
+ * 
+ */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ShipmentDetailsController {
 
 	@Autowired
@@ -28,22 +32,32 @@ public class ShipmentDetailsController {
 	@Autowired
 	private EmailService emailService;
 
+	/**
+	 * This method gets all shipment details
+	 * @return
+	 */
 	@GetMapping("/shipments")
 	public ResponseEntity<List<ShipmentDetails>> getAllShipmentDetails() {
 		try {
 			List<ShipmentDetails> shipments = new ArrayList<>();
 
+			shipmentDetailsService.getAllShipmentDetails().forEach(shipments::add);
+
 			if (shipments.isEmpty()) {
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 			}
 
-			shipmentDetailsService.getAllShipmentDetails().forEach(shipments::add);
 			return new ResponseEntity<>(shipments, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
+	/**
+	 * This method gets the shipment details count on basis of shipment id
+	 * @param id
+	 * @return
+	 */
 	@GetMapping("/shipments/{id}")
 	public ResponseEntity<ShipmentDetails> getShipmentById(@PathVariable("id") long id) {
 		Optional<ShipmentDetails> tutorialData = shipmentDetailsService.getShipmentDetailsById(id);
@@ -55,26 +69,30 @@ public class ShipmentDetailsController {
 		}
 	}
 
-	
+	/**
+	 * This method gets the shipment details count on basis of status
+	 * @param status
+	 * @return
+	 */
 	@GetMapping("/shipments/status/{status}")
 	public ResponseEntity<ShipmentCountResponse> getShipmentByStatus(@PathVariable("status") String status) {
 		List<ShipmentDetails> lst = shipmentDetailsService.getShipmentByStatus(status);
 		ShipmentCountResponse response = new ShipmentCountResponse(lst.size());
 		return ResponseEntity.ok(response);
 	}
-	
+
+	/**
+	 * This method read emails from inbox and create entry for shipment details
+	 * @return
+	 */
 	@GetMapping("/shipment")
 	public ResponseEntity<String> createShipmentDetail() {
 		try {
-			
-			//read email contents and insert data 
+			// read email contents and insert data
 			emailService.fetchEmails();
-			
 			return new ResponseEntity<>("Shipment created", HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-		
-	}
-	 
+}
